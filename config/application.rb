b34b4303fd0d05971855b12197150261ad806cec
David Looi <david@cultureamp.com>
2018-11-05 12:26:42 +1100

    Load env var from .env

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module SapCatcher
 
     config.lograge.enabled = true
 
+    require 'dotenv/load'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
